f6496b67ab74863de762ad3308cd75371bbb30f2
orianeberti <oriane.berti@gmail.com>
2022-04-13 15:32:45 +0200

    [build]: backoffice users done

--- src/Controller/BackOffice/UserController.php
+++ src/Controller/BackOffice/UserController.php
@@ -5,9 +5,11 @@ namespace App\Controller\BackOffice;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -28,7 +30,7 @@ class UserController extends AbstractController
     /**
      * @Route("/new", name="app_user_new", methods={"GET", "POST"})
      */
-    public function new(Request $request, UserRepository $userRepository): Response
+    public function new(Request $request, UserRepository $userRepository, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
@@ -36,6 +38,14 @@ class UserController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $userRepository->add($user);
+
+            if ($form->get('password')->getData()) {
+                // Si oui, on hache le nouveau mot de passe
+                $hashedPassword = $userPasswordHasher->hashPassword($user, $form->get('password')->getData());
+                // On écrase le mot de passe en clair par le mot de passe haché
+                $user->setPassword($hashedPassword);
+            }
+            $entityManager->flush();
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -58,13 +68,22 @@ class UserController extends AbstractController
     /**
      * @Route("/{id}/edit", name="app_user_edit", methods={"GET", "POST"})
      */
-    public function edit(Request $request, User $user, UserRepository $userRepository): Response
+    public function edit(Request $request, User $user,  EntityManagerInterface $entityManager, UserRepository $userRepository, UserPasswordHasherInterface $userPasswordHasher): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $userRepository->add($user);
+
+            if ($form->get('password')->getData()) {
+                // Si oui, on hache le nouveau mot de passe
+                $hashedPassword = $userPasswordHasher->hashPassword($user, $form->get('password')->getData());
+                // On écrase le mot de passe en clair par le mot de passe haché
+                $user->setPassword($hashedPassword);
+            }
+            
+            $entityManager->flush();
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
 
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,19 +3,70 @@
 namespace App\Form;
 
 use App\Entity\User;
+use FOS\RestBundle\Validator\Constraints\Regex;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('roles')
-            ->add('password')
-            ->add('email')
+            ->add('username', TextType::class)
+            ->add('roles', ChoiceType::class, [
+                'choices'  => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                ],
+                'multiple' => true,
+                'expanded' => true,
+            ])
+            ->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) {
+                // On récupère le form depuis l'event (pour travailler avec)
+                $form = $event->getForm();
+                // On récupère le user mappé sur le form depuis l'event
+                $user = $event->getData();
+
+                // On conditionne le champ "password"
+                // Si user existant, il a id non null
+                if ($user->getId() !== null) {
+                    // Edit
+                    $form->add('password', null, [
+                        // Pour le form d'édition, on n'associe pas le password à l'entité
+                        // @link https://symfony.com/doc/current/reference/forms/types/form.html#mapped
+                        'label' => 'Mot de passe',
+                        'mapped' => false,
+                        'required' => false,
+                        'attr' => [
+                            'placeholder' => 'Laissez vide si inchangé'
+                        ]
+                    ]);
+                } else {
+                    // New
+                    $form->add('password', null, [
+                        // En cas d'erreur du type
+                        // Expected argument of type "string", "null" given at property path "password".
+                        // (notamment à l'edit en cas de passage d'une valeur existante à vide)
+                        'label' => 'Mot de passe',
+                        'empty_data' => '',
+                        // On déplace les contraintes de l'entité vers le form d'ajout
+                        'constraints' => [
+                            new NotBlank(),
+                            new Regex(
+                                "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/",
+                                "Le mot de passe doit contenir au minimum 8 caractères, une majuscule, un chiffre et un caractère spécial"
+                            ),
+                        ],
+                    ]);
+                }
+            })
+            ->add('email', TextType::class)
         ;
     }
 
