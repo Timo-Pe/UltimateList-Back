071b02e6b165b9222e0668e89c1bc5e046fa152f
orianeberti <oriane.berti@gmail.com>
2022-04-17 19:33:02 +0200

    [fix]: server test to login

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -284,7 +284,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface, Equatab
     //    return false;
     //}
 
-    if ($this->username !== $user->getUsername()) {
+    if ($this->username !== $user->getUserIdentifier()) {
         return false;
     }
 
